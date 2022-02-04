d6ad9191dead26fd408f4c67913d27126cf9d2b2
codeuronline <j.kasperki@codeur.online>
2022-02-04 09:38:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo "echo # projet4 >> README.md
+    git init
+    git add README.md
+    git commit -m 'first commit'
+    git branch -M main
+    git remote add origin 'https://github.com/Cocci38/projet4.git'
+    git push -u origin main";
+    ?>
+</body>
+
+</html>
