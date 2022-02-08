b050c48f2375e42a98af3896695f7eacc4eb6711
hakime-AI <hakimeattoumani.simplon@gmail.com>
2022-02-08 09:36:45 +0100

    IMPLEMENTATION MVC

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+require "../vendor\autoload.php";
+require "../vendor\altorouter\altorouter\AltoRouter.php";
+$uri=$_SERVER['REQUEST_URI'];
+$router = new AltoRouter();
+
+// map homepage
+// $router->map( 'GET', '/projet4', function() {
+// 	require __DIR__ . '/views/acceuil.php';
+// });
+
+$router->map( 'GET', '/projet4/', 'acceuil','acceuil'); // GET,URL,ADDRESSE,NOM
+$router->map( 'GET', '/projet4/annonce/[*:slug]-[i:id]','traitement');
+$router->map( 'GET', '/projet4/blog/[*:slug]-[i:id]','blog/annonce');
+
+$match = $router->match();
+
+    if(is_array($match)){
+        require "../app/views/elements/header.php";
+        if(is_callable($match['target'])){
+            call_user_func_array($match['target'],$match['params']);
+        }else{
+            $params=$match['params'];
+            require "../app/views/{$match['target']}.php";
+        }
+    }else{
+        require "../app/views/error.php";
+    }
+    require "../app/views/elements/footer.php";
+    // $match['target']();
+
+
+?>
