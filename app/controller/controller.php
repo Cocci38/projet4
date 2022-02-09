86e8ac69779f9755c4d3bba3039b99b06c1a9736
hakime-AI <hakimeattoumani.simplon@gmail.com>
2022-02-09 09:09:34 +0100

    ajout commentaire

--- app/controller/controller.php
+++ app/controller/controller.php
@@ -5,13 +5,13 @@
         }else{
             $params=$match['params'];
             ob_start();
-            require "../app/views/{$match['target']}.php";
+            require "../app/views/{$match['target']}View.php";
             $pageContent=ob_get_clean();
         }
 
         require "../app/views/elements/layout.php";
     }else{
-        require "../app/views/error.php";
+        require "../app/views/errorView.php";
     }
     
 ?>
