aacb95dc34bef8011448331be709b5af8d2deff9
hakime-AI <hakimeattoumani.simplon@gmail.com>
2022-02-15 10:50:18 +0100

    maj public/index

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
-require "../vendor\autoload.php";
-require "../vendor\altorouter\altorouter\AltoRouter.php";
+require "../vendor/altorouter/altorouter/AltoRouter.php";
 $uri=$_SERVER['REQUEST_URI'];
 $router = new AltoRouter();
 
@@ -9,25 +8,22 @@ $router = new AltoRouter();
 // 	require __DIR__ . '/views/acceuil.php';
 // });
 
-$router->map( 'GET', '/projet4/', 'acceuil','acceuil'); // GET,URL,ADDRESSE,NOM
-$router->map( 'GET', '/projet4/annonce/[*:slug]-[i:id]','annonce');
-$router->map( 'GET', '/projet4/blog/[*:slug]-[i:id]','blog/annonce');
+$router->map( 'GET|POST', '/projet4/', 'acceuil','acceuil'); // GET,URL,ADDRESSE,NOM
+$router->map( 'GET|POST', '/projet4/ajouter','ajouter','ajouter');
+$router->map( 'GET|POST', '/projet4/annonce','annonce','annonce');
+$router->map( 'GET|POST', '/projet4/error','error','error');
+$router->map( 'GET|POST', '/projet4/validation','validation','validation');
+
+$router->map( 'GET|POST', '/projet4/ajouter/[*:slug]','ajouter','ajouterPerso');
+$router->map( 'GET|POST', '/projet4/annonce/[*:slug]','annonce','annoncePerso');
+$router->map( 'GET|POST', '/projet4/error/[*:slug]','error');
+$router->map( 'GET|POST', '/projet4/validation/[*:slug]','validation','validationPerso');
 
 $match = $router->match();
 
-    if(is_array($match)){
-        require "../app/views/elements/header.php";
-        if(is_callable($match['target'])){
-            call_user_func_array($match['target'],$match['params']);
-        }else{
-            $params=$match['params'];
-            require "../app/views/{$match['target']}.php";
-        }
-    }else{
-        require "../app/views/error.php";
-    }
-    require "../app/views/elements/footer.php";
+require "../app/controller/controller.php";
     // $match['target']();
 
 
 ?>
+
